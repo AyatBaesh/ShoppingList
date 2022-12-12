c2b90226de9a4ebff485bc359409dc47e72b5426
Ayat <ayatbaeshev@gmail.com>
2022-12-12 22:27:04 +0100

    added comments in code

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
+//HTMLElement.insertAdjacentHTML(position, html) is used to add html to the html element at certain position
+//HTMLElement.textContent - used to add text content to an html element
 import { addItem, lowerCasedCSV } from "./functions.js";
-let shoppingList = document.querySelector("#shopping-list");
+
+let shoppingList = document.querySelector("#shopping-list");//used to add items to the list
 let itemInput = document.querySelector("#item-name");
-let listCSV = document.querySelector("#exported-list");
+let listCSV = document.querySelector("#exported-list");//used to output list items in CSV format
 let form = document.querySelector("#addForm");
 let items = [];
 
 function renderItems(items){
-    shoppingList.innerHTML = "";
+    shoppingList.innerHTML = "";//needed to refresh list so it doesnt doubles
     items.forEach((item) =>{
     shoppingList.insertAdjacentHTML('beforeend', `<li>${item}</li>`);
     });
@@ -18,7 +21,6 @@ form.addEventListener("submit", event => {
     addItem(items, itemInput.value);
     itemInput.value = "";
     renderItems(items);
-    // items =[];
     
 
 } )
